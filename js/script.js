59d6d40dd0ed3f38052298282ad1512af9cec5a7
bezawitsebsbe <sebsbebezawit@gmail.com>
2025-08-19 20:08:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navlist');
+
+menu.onclick=() =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+window.onscroll=() =>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}*/
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = function() {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = function() {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
